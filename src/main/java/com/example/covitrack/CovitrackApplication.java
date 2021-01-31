72234d42b62a2afe94529bd70e811dbcd43e25d3
Tabrez Shaikh <tabrezshaikh17970@gmail.com>
2021-01-31 12:00:29 +0530

    First commit with default spring boot starter.

---
+++ src/main/java/com/example/covitrack/CovitrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.covitrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovitrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovitrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/covitrack/CovitrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.covitrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovitrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
